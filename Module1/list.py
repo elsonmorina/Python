f372ae6aec76e25d5f76719819de886854c7719c
elsonmorina <167892030+elsonmorina@users.noreply.github.com>
2025-11-08 14:32:34 +0100

    module1

---
+++ Module1/list.py
@@ -0,0 +1,24 @@
+to_do_list = ["math homework", "clean the room","research the history","fun"]
+print(to_do_list[0])
+print(to_do_list[1])
+print(to_do_list[2])
+print(to_do_list[3])
+
+shopping_list = ["apples","bananas",3,4.5]
+shopping_list.append("oranges")
+print(shopping_list)
+
+shopping_list.insert(2,"lemons")
+print(shopping_list)
+
+to_do_list.remove("research the history")
+print(to_do_list)
+
+del to_do_list[2]
+print(to_do_list)
+
+to_do_list[0] = "physics homewrok"
+print(to_do_list)
+
+to_do_list[1] = 3
+print(to_do_list)
---
+++ Module1/variable.py
@@ -0,0 +1,22 @@
+name = "Elsoni"
+print(name)
+
+temperature = 98.6
+print(temperature)
+print(type(temperature))
+
+age = 30
+print("Age is:",age)
+
+x = 5
+y = 3
+result = x + y
+print("Rezultati eshte:",result)
+
+age = age + 1
+print("Age now is:", age)
+
+first_name = "John"
+last_name = "Doe"
+full_name = first_name + " " + last_name
+print(full_name)
