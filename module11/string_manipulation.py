3081cc48bde44af8cf579934ee63a160fa3f8498
unknown <Student@DIGITALSCHOOL.LOCAL>
2026-02-28 14:04:57 +0100

    module11

---
+++ module11/string_manipulation.py
@@ -0,0 +1,16 @@
+with open('example.txt','r') as file:
+    for line in file:
+        cleaned_lines = line.strip()#remove whitespace
+        print(cleaned_lines)
+
+#splitting lines into words
+with open('example.txt','r') as file:
+    for line in file:
+        words = line.strip().split()
+        print(words)
+
+name = "Alice"
+age = 30
+with open('output.txt','w')as file:
+    file.write("Name:"+name+"\n")
+    file.write("Age:" + str(age)+ "\n")
